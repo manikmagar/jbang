1e1c64f576028d171205b5650b522f87b86f5d01
Max Rydahl Andersen <max@xam.dk>
2020-01-19 21:37:27 +0100

    files used for choco review

--- src/main/scripts/choco/tools/chocolateyuninstall.ps1
+++ src/main/scripts/choco/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$jbang_home = Join-Path $package 'jbang-@projectVersion@'
+$jbang_home = Join-Path $package 'jbang-0.6.0.5'
 $jbang_bat = Join-Path $jbang_home 'bin/jbang.bat'
 
 Uninstall-BinFile -Name 'jbang' -Path $jbang_bat
